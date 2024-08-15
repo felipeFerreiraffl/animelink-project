41452acb25ff643d25bb3e19d63da50e58828b0d
Felipe Ferreira <felipado2x0@gmail.com>
2024-08-15 13:24:17 -0300

    create react app to make test on signup screen

---
+++ src/components/pages/SignUp/SignUpArea/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+
+export function AreaCadastro() {
+    return(
+        <div className="cadastro-area"> 
+            <h1 className="cadastro-area-titulo">Cadastro</h1>
+        </div>
+    );
+}
---
+++ src/components/pages/SignUp/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import { AreaCadastro } from "./SignUpArea";
+
+export default function Cadastro() {
+    return(
+        <div className="cadastro-pagina">
+            <AreaCadastro />
+        </div>
+    );
+}
