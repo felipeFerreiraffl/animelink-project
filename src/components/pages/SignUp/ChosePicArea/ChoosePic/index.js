c8b048df7d2d318d04410afc90ff5682542cf3fa
Felipe Ferreira <felipado2x0@gmail.com>
2024-08-16 22:09:30 -0300

    docs: completed a dumb sign up page (with no interactions)

--- src/components/pages/SignUp/ChosePicArea/ChoosePic/index.js
+++ src/components/pages/SignUp/ChosePicArea/ChoosePic/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "./styles.css";
 import hunterPic from "../../../../assets/Images/ProfilePics/hunter-hunter.png"
-import bnhaPic from "../../../../assets/Images/ProfilePics/boku-no-hero.png"
-import pokemonPic from "../../../../assets/Images/ProfilePics/pokemon.png"
+import bnhaPic from "../../../../assets//Images/ProfilePics/boku-no-hero.png"
+import pokemonPic from "../../../../assets/Images/ProfilePics/pokemon.png";
+import esquerda from "../../../../assets/Icons/left-arrow.svg"; 
+import direita from "../../../../assets/Icons/right-arrow.svg"; 
 
 export function AreaEscolhaImagem() {
     return(
         <div className="imagem-escolha-area">
+            <img src={esquerda} alt="Esquerda" width={40} /> 
             <img src={hunterPic} alt="img1" />
             <img src={bnhaPic} alt="img2" />
             <img src={pokemonPic} alt="img3" />
+            <img src={direita} alt="Esquerda" width={40} /> 
         </div>
     );
 }
--- src/components/pages/SignUp/SignUpArea/index.js
+++ src/components/pages/SignUp/SignUpArea/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import "./styles.css";
 import { CamposCadastro } from "./SignUpLabels";
 import { BotaoCadastro } from "./SignUpButton";
+import fecharCadastro from "../../../assets/Icons/close-button.svg";
 
 export function AreaCadastro() {
     return(
-        <div className="cadastro-area"> 
+        <div className="cadastro-area">
+            <div className="cadastro-icone-fechar-area">
+                <img className="cadastro-icone-fechar" src={fecharCadastro} width={24} alt="Fechar" />
+            </div> 
             <h1 className="cadastro-area-titulo">Cadastro</h1>
             <CamposCadastro title={"Nome completo"} placeholder={"Seu nome"} type={"text"} />
             <CamposCadastro title={"Data de nascimento"} type={"date"} />
