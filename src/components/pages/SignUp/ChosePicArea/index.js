9f4c89e2ed6b293c51babd2de47b5dc54244b53c
Felipe Ferreira <felipado2x0@gmail.com>
2024-08-17 21:08:10 -0300

    docs: finished sign up page

--- src/components/pages/SignUp/ChosePicArea/index.js
+++ src/components/pages/SignUp/ChosePicArea/index.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import profilePic from "../../../assets/SignUp/profile-pic-choosen.png"
 import { AreaEscolhaImagem } from "./ChoosePic";
 
 export function AreaImagemPerfil() {
+    const [imgSelecionada, setImgSelecionada] = useState(null);
+    const [trocaAnimacao, setTrocaAnimacao] = useState(false);
+
+    const trocarDeImagem = (img) => {
+        setTrocaAnimacao(true);
+
+        setTimeout(() => {
+            setImgSelecionada(img);
+            setTrocaAnimacao(false);
+        }, 300);
+    };
+
     return(
         <div className="imagem-perfil-area">
             <h1 className="imagem-perfil-titulo">Escolha uma foto de perfil</h1>
-            <img src={profilePic} alt="Imagem de perfil" />
-            <AreaEscolhaImagem />
+            <img className={ `imagem-perfil-preview ${trocaAnimacao ? "imagem-perfil-animacao" : ""}` } src={imgSelecionada || profilePic} alt="Imagem de perfil" />
+            <AreaEscolhaImagem setImgSelecionada={trocarDeImagem} />
         </div>
     );
 }
